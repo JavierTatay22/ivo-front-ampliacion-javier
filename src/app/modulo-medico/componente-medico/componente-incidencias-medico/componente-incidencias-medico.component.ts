bf849a3c886e4de78bfb49eead8f9b5bd0436880
JavierTatayPonce <jtataypo@gmail.com>
2024-06-02 18:37:53 +0200

    Delete src directory

--- src/app/modulo-medico/componente-medico/componente-incidencias-medico/componente-incidencias-medico.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-componente-incidencias-medico',
-  templateUrl: './componente-incidencias-medico.component.html',
-  styleUrls: ['./componente-incidencias-medico.component.css']
-})
-export class ComponenteIncidenciasMedicoComponent {
-
-}
